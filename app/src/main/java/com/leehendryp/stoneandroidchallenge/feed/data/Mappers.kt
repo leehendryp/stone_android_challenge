17eb615641899f8bac35fe452e0b6b2eb1840759
leehendryp <leehendryp@gmail.com>
2020-04-28 10:38:53 -0300

    feat(mappers): add functions to convert object between clean layers

--- app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/Mappers.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/Mappers.kt
@@ -1,6 +1,7 @@
 package com.leehendryp.stoneandroidchallenge.feed.data
 
 import com.leehendryp.stoneandroidchallenge.feed.data.entities.JokeResponse
+import com.leehendryp.stoneandroidchallenge.feed.data.local.database.RoomJoke
 import com.leehendryp.stoneandroidchallenge.feed.domain.model.Joke
 
 fun List<JokeResponse>.toJokeList(): List<Joke> {
@@ -13,4 +14,16 @@ fun JokeResponse.toJoke() = Joke(
     categories = categories ?: listOf(),
     updatedAt = updatedAt ?: "",
     value = value ?: ""
+)
+
+fun RoomJoke.toJoke() = Joke(
+    categories = categories,
+    updatedAt = updatedAt,
+    value = value
+)
+
+fun Joke.toRoomJoke() = RoomJoke(
+    categories = categories,
+    updatedAt = updatedAt,
+    value = value
 )
