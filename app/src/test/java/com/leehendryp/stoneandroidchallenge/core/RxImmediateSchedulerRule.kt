910da369883aa6d4ee98aa797f1a82a97f771eb3
leehendryp <leehendryp@gmail.com>
2020-04-29 21:36:00 -0300

    test(rxjava): improve base test class for synchronous rx calls

--- app/src/test/java/com/leehendryp/stoneandroidchallenge/core/RxImmediateSchedulerRule.kt
+++ app/src/test/java/com/leehendryp/stoneandroidchallenge/core/RxImmediateSchedulerRule.kt
@@ -1,22 +1,44 @@
 package com.leehendryp.stoneandroidchallenge.core
 
 import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.internal.schedulers.ExecutorScheduler.ExecutorWorker
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
-import io.reactivex.rxjava3.schedulers.Schedulers
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 
 class RxImmediateSchedulerRule : TestRule {
+    private val immediateScheduler = object : Scheduler() {
+        @NonNull
+        override fun scheduleDirect(
+            @NonNull run: Runnable,
+            delay: Long,
+            @NonNull unit: TimeUnit
+        ): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
 
-    override fun apply(base: Statement, description: Description): Statement {
+        @NonNull
+        override fun createWorker(): Worker {
+            return ExecutorWorker(Executor { it.run() }, false, false)
+        }
+    }
+
+    @NonNull
+    override fun apply(@NonNull base: Statement, @NonNull description: Description): Statement {
         return object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
-                RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
-                RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
-                RxJavaPlugins.setNewThreadSchedulerHandler { Schedulers.trampoline() }
-                RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+                RxJavaPlugins.setInitIoSchedulerHandler { immediateScheduler }
+                RxJavaPlugins.setInitComputationSchedulerHandler { immediateScheduler }
+                RxJavaPlugins.setInitNewThreadSchedulerHandler { immediateScheduler }
+                RxJavaPlugins.setInitSingleSchedulerHandler { immediateScheduler }
+                RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediateScheduler }
 
                 try {
                     base.evaluate()
