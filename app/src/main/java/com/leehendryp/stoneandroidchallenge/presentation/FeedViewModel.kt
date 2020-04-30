dba203913d931f260e1a571bc87313d90a2ba132
leehendryp <leehendryp@gmail.com>
2020-04-30 12:30:27 -0300

    fix(view-model): add composite disposable call

--- app/src/main/java/com/leehendryp/stoneandroidchallenge/presentation/FeedViewModel.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/presentation/FeedViewModel.kt
@@ -5,32 +5,30 @@ import com.leehendryp.stoneandroidchallenge.core.extensions.observeOnMain
 import com.leehendryp.stoneandroidchallenge.core.extensions.subscribeOnIO
 import com.leehendryp.stoneandroidchallenge.feed.domain.usecases.SearchJokeUseCase
 import com.leehendryp.stoneandroidchallenge.presentation.FeedState.Default
-import com.leehendryp.stoneandroidchallenge.presentation.FeedState.Error
-import com.leehendryp.stoneandroidchallenge.presentation.FeedState.Loading
-import com.leehendryp.stoneandroidchallenge.presentation.FeedState.Success
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import javax.inject.Inject
 
 class FeedViewModel @Inject constructor(
+    private val compositeDisposable: CompositeDisposable,
     private val searchJokeUseCase: SearchJokeUseCase
 ) : ViewModel() {
-    @Inject
-    lateinit var compositeDisposable: CompositeDisposable
 
     val state by lazy { BehaviorSubject.create<FeedState>().apply { onNext(Default) } }
 
     fun search(query: String) {
-        searchJokeUseCase.execute(query)
-            .subscribeOnIO()
-            .observeOnMain()
-            .doOnSubscribe { state(Loading) }
-            .doFinally { state(Default) }
-            .subscribeBy(
-                onSuccess = { jokeList -> state(Success(jokeList)) },
-                onError = { error -> state(Error(error)) }
-            )
+        compositeDisposable.add(
+            searchJokeUseCase.execute(query)
+                .subscribeOnIO()
+                .observeOnMain()
+                .doOnSubscribe { state(FeedState.Loading) }
+                .doFinally { state(Default) }
+                .subscribeBy(
+                    onSuccess = { jokeList -> state(FeedState.Success(jokeList)) },
+                    onError = { error -> state(FeedState.Error(error)) }
+                )
+        )
     }
 
     private fun state(feedState: FeedState) = state.onNext(feedState)
