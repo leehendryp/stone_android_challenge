cd62d8fe04455a4352db2e7351e8004d4a286f5f
leehendryp <leehendryp@gmail.com>
2020-05-01 20:21:28 -0300

    refactor(extensions): remove uneeded ones

--- app/src/main/java/com/leehendryp/stoneandroidchallenge/core/extensions/ViewExtensions.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/core/extensions/ViewExtensions.kt
@@ -1,6 +1,33 @@
 package com.leehendryp.stoneandroidchallenge.core.extensions
 
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.widget.SearchView
+
+fun View.visible() {
+    visibility = VISIBLE
+}
+
+fun View.gone() {
+    visibility = GONE
+}
+
+fun View.vanish(duration: Long = 300) {
+    animate()
+        .alpha(0f)
+        .setDuration(duration)
+        .withEndAction { gone() }
+        .start()
+}
+
+fun View.fadeIn(duration: Long = 300) {
+    animate()
+        .alpha(1f)
+        .setDuration(duration)
+        .withStartAction { visible() }
+        .start()
+}
 
 fun SearchView.doOnQuerySubmit(block: (String) -> Unit) {
     setOnQueryTextListener(object : SearchView.OnQueryTextListener {
