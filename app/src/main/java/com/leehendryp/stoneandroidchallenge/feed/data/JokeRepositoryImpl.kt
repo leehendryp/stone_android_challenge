5269ec7b954eb5e4be4c6fba4a5a1daa42100c3d
leehendryp <leehendryp@gmail.com>
2020-05-04 14:36:55 -0300

    feat(search-results): add flowable to allow back pressure on feature

--- app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/JokeRepositoryImpl.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/JokeRepositoryImpl.kt
@@ -6,8 +6,7 @@ import com.leehendryp.stoneandroidchallenge.feed.data.remote.RemoteDataSource
 import com.leehendryp.stoneandroidchallenge.feed.domain.JokeRepository
 import com.leehendryp.stoneandroidchallenge.feed.domain.model.Joke
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Maybe
-import java.util.concurrent.TimeUnit
+import io.reactivex.rxjava3.core.Flowable
 import javax.inject.Inject
 
 class JokeRepositoryImpl @Inject constructor(
@@ -15,26 +14,16 @@ class JokeRepositoryImpl @Inject constructor(
     private val remoteSource: RemoteDataSource,
     private val localSource: LocalDataSource
 ) : JokeRepository {
-    companion object {
-        private const val BUFFER_CAPACITY = 20
-    }
-
-    override fun search(query: String): Maybe<List<Joke>> {
+    override fun search(query: String): Flowable<Joke> {
         return if (networkUtils.isInternetAvailable()) remoteSearch(query)
-            .doAfterSuccess { response -> response.forEach { save(it) } }
+            .doOnNext { save(it) }
         else localSearch(query)
     }
 
     override fun save(joke: Joke): Completable = localSource.save(joke)
 
-    private fun remoteSearch(query: String): Maybe<List<Joke>> = remoteSource.search(query)
-        .flatMap { response -> Maybe.just(response.toJokeList()) }
+    private fun remoteSearch(query: String): Flowable<Joke> = remoteSource.search(query)
+        .map { response -> response.toJoke() }
 
-    private fun localSearch(query: String): Maybe<List<Joke>> {
-        return localSource.search(query)
-            .timeout(10, TimeUnit.SECONDS)
-            .onBackpressureBuffer(BUFFER_CAPACITY)
-            .toList()
-            .flatMapMaybe { Maybe.just(it) }
-    }
+    private fun localSearch(query: String): Flowable<Joke> = localSource.search(query)
 }
--- app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/domain/usecases/SearchJokeUseCaseImpl.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/domain/usecases/SearchJokeUseCaseImpl.kt
@@ -2,11 +2,11 @@ package com.leehendryp.stoneandroidchallenge.feed.domain.usecases
 
 import com.leehendryp.stoneandroidchallenge.feed.domain.JokeRepository
 import com.leehendryp.stoneandroidchallenge.feed.domain.model.Joke
-import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Flowable
 import javax.inject.Inject
 
 class SearchJokeUseCaseImpl @Inject constructor(
     private val repository: JokeRepository
 ) : SearchJokeUseCase {
-    override fun execute(query: String): Maybe<List<Joke>> = repository.search(query)
+    override fun execute(query: String): Flowable<Joke> = repository.search(query)
 }
