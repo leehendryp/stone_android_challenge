32a2ca9d803e55a40be02d3b146845accddb8aca
leehendryp <leehendryp@gmail.com>
2020-05-04 14:33:25 -0300

    test(search): replace maybe with flowable based on abstraction changes

--- app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/leehendryp/stoneandroidchallenge/feed/data/remote/RemoteDataSource.kt
@@ -1,8 +1,8 @@
 package com.leehendryp.stoneandroidchallenge.feed.data.remote
 
 import com.leehendryp.stoneandroidchallenge.feed.data.entities.JokeResponse
-import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Flowable
 
 interface RemoteDataSource {
-    fun search(query: String): Maybe<List<JokeResponse>>
+    fun search(query: String): Flowable<JokeResponse>
 }
